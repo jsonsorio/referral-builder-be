92cd1ad5234bed290bba4d4eb63a7328b8b0c78b
jsonsorio <json.sorio@gmail.com>
2023-10-11 18:23:43 +0800

    refactor: updated seed.ts

--- src/utils/seed.ts
+++ src/utils/seed.ts
@@ -17,18 +17,18 @@ interface Referral {
 export default function generateFakeReferral(num: number): Referral[] {
     const referrals: Referral[] = [];
 
-    const firstname = faker.person.firstName();
-    const lastname = faker.person.lastName();
-    const email = faker.internet.email();
-    const phone = faker.helpers.replaceSymbolWithNumber('0##########');
-    const addressline1 = faker.location.streetAddress();
-    const addressline2 = faker.location.streetAddress();
-    const suburb = faker.location.city();
-    const state = faker.location.state();
-    const postcode = faker.location.zipCode();
-    const country = faker.location.country();
-
     for (let i = 0; i < num; i++) {
+        const firstname = faker.person.firstName();
+        const lastname = faker.person.lastName();
+        const email = faker.internet.email();
+        const phone = faker.helpers.replaceSymbolWithNumber('0##########');
+        const addressline1 = faker.location.streetAddress();
+        const addressline2 = faker.location.streetAddress();
+        const suburb = faker.location.city();
+        const state = faker.location.state();
+        const postcode = faker.location.zipCode();
+        const country = faker.location.country();
+
         referrals.push({firstname, lastname, email, phone, addressline1, addressline2, suburb, state, postcode, country});
     }
 
